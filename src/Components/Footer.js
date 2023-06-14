6cf9e1eda1a697b86ee8d4ad6b4134bbbc4cf92a
Aman Bhadani <bhadani.aman.911@gmail.com>
2023-06-14 12:27:50 +0530

    initial push

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+      <h4 className="p-10 text-center bg-yellow-200 h-px">
+        This site is developed by{" "}
+        <a
+          className="text-red-400"
+          href="https://www.linkedin.com/in/aman-bhadani-0bb4bb206/"
+          target="_blank"
+        >
+          Aman Bhadani
+        </a>
+      </h4>
+    );
+  };
+  
+  export default Footer;
